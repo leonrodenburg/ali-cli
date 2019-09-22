33c30322ae8fadefc50ea238532dc56dc5e6b2a6
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-09-22 14:37:58 +0800

    Add test for key generation

---
+++ tests/unit/ali/commands/crypto/test_init.py
@@ -0,0 +1,30 @@
+import os
+import click
+import tempfile
+import shutil
+
+from unittest import mock
+from click.testing import CliRunner
+
+from ali.commands.crypto import crypto
+
+
+mock_obj = {"client": {}}
+
+
+@mock.patch("ali.commands.crypto.Fernet")
+def test_generate_key(mock_fernet):
+    mock_fernet.generate_key.return_value = b"my-fancy-key"
+    keyfile_path = os.path.join(os.getcwd(), ".test.key")
+
+    runner = CliRunner()
+    result = runner.invoke(crypto, ["generate-key", "-k", keyfile_path], obj=mock_obj)
+
+    assert result.exception is None
+    mock_fernet.generate_key.assert_called_once()
+
+    with open(keyfile_path, "rb") as key:
+        try:
+            assert key.read() == b"my-fancy-key"
+        finally:
+            os.remove(keyfile_path)
